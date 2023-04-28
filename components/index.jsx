47cf1e658cdb48a90b8d2123e6a943ae1bcbf7dc
tuyen-icd <120370792+tuyen-icd@users.noreply.github.com>
2023-04-28 13:11:22 +0700

    push code

---
+++ components/index.jsx
@@ -0,0 +1,30 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from './DetailsDesc';
+import {
+  NFTTile,
+  EthPrice,
+  ImageCmp,
+  People,
+  EndDate,
+  SubInfo,
+} from "./SubInfo";
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  NFTTile,
+  EthPrice,
+  ImageCmp,
+  People,
+  EndDate,
+  SubInfo,
+  DetailsDesc,
+  DetailsBid
+};
